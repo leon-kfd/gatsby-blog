c461d0ff22b98d18ce779b94b679a7a95fe6c65c
leon-kfd <347040447@qq.com>
2020-03-14 19:46:56 +0800

    fix ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,8 @@
 const React = require("react")
 exports.wrapRootElement = ({ element }) => {
+  const hour = new Date().getHours()
+  const theme = hour >= 7 && hour <= 18 ? 'light' : 'dark'
+  localStorage.setItem('theme', theme)
   return (
     <div id="root">
       {element}
