b37373256602228590b5b4ae074e5f04e738dcb1
leon-kfd <347040447@qq.com>
2020-03-14 20:03:05 +0800

    remove ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,11 @@
-const React = require("react")
-exports.wrapRootElement = ({ element }) => {
-  const hour = new Date().getHours()
-  const theme = hour >= 7 && hour <= 18 ? 'light' : 'dark'
-  localStorage.setItem('theme', theme)
-  return (
-    <div id="root">
-      {element}
-    </div>
-  )
-}
+// const React = require("react")
+// exports.wrapRootElement = ({ element }) => {
+//   const hour = new Date().getHours()
+//   const theme = hour >= 7 && hour <= 18 ? 'light' : 'dark'
+//   localStorage.setItem('theme', theme)
+//   return (
+//     <div id="root">
+//       {element}
+//     </div>
+//   )
+// }
