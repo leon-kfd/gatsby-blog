811f8a9ebb614aed87c55f0a3f42bd48d4cac068
leon-kfd <347040447@qq.com>
2020-03-21 17:08:46 +0800

    add requestAnimationFrame兼容

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -5,6 +5,29 @@
 * @param {object} selector 滚动条不在body上时，需传入当前滚动条所在javascriptDom元素
 */
 export function scrollTo (top, duration, selector = window) {
+  // 兼容requestionAnimationFrame
+  (function () {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+      window.cancelAnimationFrame =
+        window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+      window.requestAnimationFrame = function (callback) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+          timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = function (id) {
+        clearTimeout(id);
+      };
+  }());
   const lastTop = selector === window ? (document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop) : selector.scrollTop
   const startTime = new Date()
   let timer
