3fd0b0c789380ffeffec2949f6f8f531f41471b0
Leon-kfd <kfd_personal@163.com>
2021-10-25 21:16:32 +0800

    doc: update howdz introduce

--- src/components/project.js
+++ src/components/project.js
@@ -51,12 +51,12 @@ const howdyLink = [
     link: 'https://github.com/leon-kfd/howdyjs/'
   },
   {
-    name: '💾NPM',
-    link: 'https://www.npmjs.com/search?q=%40howdyjs'
-  },
-  {
     name: '📖Document',
     link: 'https://kongfandong.cn/howdy/'
+  },
+  {
+    name: '💾NPM',
+    link: 'https://www.npmjs.com/search?q=%40howdyjs'
   }
 ]
 
@@ -66,8 +66,16 @@ const dashboardLink = [
     link: 'https://github.com/leon-kfd/Dashboard/'
   },
   {
-    name: '💡Demo',
+    name: '📖Document',
+    link: 'https://howdz.vercel.app'
+  },
+  {
+    name: '💾Online',
     link: 'https://s.kongfandong.cn'
+  },
+  {
+    name: '💻Chrome extension',
+    link: 'https://chrome.google.com/webstore/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/ggglfehkglgpenacfalffmiojghklamm/related'
   }
 ]
 
@@ -77,6 +85,35 @@ export default () => {
       marginBottom: '3rem'
     }}>
       <p className="home-title">Project</p>
+      <article className="home-article">
+        <header>
+          <h3
+            style={{
+              marginBottom: rhythm(1 / 4),
+            }}>
+            <a style={{ boxShadow: `none` }} href={`https://s.kongfandong.cn`}
+              target="_blank" rel="noopener noreferrer" className="home-outlink">
+              Howdz 🔸 浏览器起始页
+              <svg className="home-outlink-img" viewBox="0 0 1024 1024">
+                <path d="M912 1008.512H15.488V112h448.256v96H111.488v704.512H816V560.256h96z"></path>
+                <path d="M918.208 37.888l67.904 67.904L545.984 545.92l-67.904-67.84z"></path>
+                <path d="M1007.168 310.656h-96V112h-208V16h304z"></path>
+              </svg>
+            </a>
+          </h3>
+          <small className="outlink">
+            {dashboardLink.map(item => <a href={item.link} target="_blank" rel="noopener noreferrer">{item.name}</a>)}
+          </small>
+        </header>
+        <section>
+          <p>个性化的浏览器导航首页面板，支持自定义添加组件，可编辑组件各种属性。响应式设计，可自定义随机壁纸背景图及各种可配置组件，并提供
+            <a href="https://s.kongfandong.cn" target="_blank">在线网页版</a>、
+            <a href="https://chrome.google.com/webstore/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/ggglfehkglgpenacfalffmiojghklamm/related" target="_blank">Chrome插件</a>、
+            <a href="https://microsoftedge.microsoft.com/addons/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/cgcggcdgjfmeoemmdpleinicgepijegd" target="_blank">Edge插件</a>和
+            <a href="https://github.com/leon-kfd/Dashboard/releases/tag/v1.0.1-beta.2" target="_blank">桌面应用</a>等各种使用方式。</p>
+        </section>
+      </article>
+
       <article className="home-article">
         <header>
           <h3
@@ -114,31 +151,6 @@ export default () => {
               }
             </ul>
           </details>
-        </section>
-      </article>
-
-      <article className="home-article">
-        <header>
-          <h3
-            style={{
-              marginBottom: rhythm(1 / 4),
-            }}>
-            <a style={{ boxShadow: `none` }} href={`https://s.kongfandong.cn`}
-              target="_blank" rel="noopener noreferrer" className="home-outlink">
-              Howdz 🔸 浏览器起始页
-              <svg className="home-outlink-img" viewBox="0 0 1024 1024">
-                <path d="M912 1008.512H15.488V112h448.256v96H111.488v704.512H816V560.256h96z"></path>
-                <path d="M918.208 37.888l67.904 67.904L545.984 545.92l-67.904-67.84z"></path>
-                <path d="M1007.168 310.656h-96V112h-208V16h304z"></path>
-              </svg>
-            </a>
-          </h3>
-          <small className="outlink">
-            {dashboardLink.map(item => <a href={item.link} target="_blank" rel="noopener noreferrer">{item.name}</a>)}
-          </small>
-        </header>
-        <section>
-          <p>个性化的浏览器导航首页面板，支持自定义添加组件，可编辑组件各种属性。响应式设计，可自定义随机壁纸背景图，目前支持时钟、天气、搜索栏、TodoList等组件。</p>
         </section>
       </article>
     </div>
