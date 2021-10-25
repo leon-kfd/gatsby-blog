9134a2978f18ce3a579cd84b533c817fd64940db
Leon-kfd <kfd_personal@163.com>
2021-10-25 21:36:42 +0800

    doc: update project show

--- src/components/project.js
+++ src/components/project.js
@@ -45,6 +45,25 @@ const howdyComponent = [
   return item
 })
 
+const howdzDashboard = [
+  {
+    title: '在线网页版',
+    link: 'https://s.kongfandong.cn'
+  },
+  {
+    title: 'Chrome插件',
+    link: 'https://chrome.google.com/webstore/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/ggglfehkglgpenacfalffmiojghklamm/related'
+  },
+  {
+    title: 'Edge插件',
+    link: 'https://microsoftedge.microsoft.com/addons/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/cgcggcdgjfmeoemmdpleinicgepijegd'
+  },
+  {
+    title: '桌面应用',
+    link: 'https://github.com/leon-kfd/Dashboard/releases/tag/v1.0.1-beta.2'
+  }
+]
+
 const howdyLink = [
   {
     name: '⚡Github',
@@ -72,10 +91,6 @@ const dashboardLink = [
   {
     name: '💾Online',
     link: 'https://s.kongfandong.cn'
-  },
-  {
-    name: '💻Chrome extension',
-    link: 'https://chrome.google.com/webstore/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/ggglfehkglgpenacfalffmiojghklamm/related'
   }
 ]
 
@@ -106,11 +121,21 @@ export default () => {
           </small>
         </header>
         <section>
-          <p>个性化的浏览器导航首页面板，支持自定义添加组件，可编辑组件各种属性。响应式设计，可自定义随机壁纸背景图及各种可配置组件，并提供
-            <a href="https://s.kongfandong.cn" target="_blank">在线网页版</a>、
-            <a href="https://chrome.google.com/webstore/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/ggglfehkglgpenacfalffmiojghklamm/related" target="_blank">Chrome插件</a>、
-            <a href="https://microsoftedge.microsoft.com/addons/detail/howdz%E8%B5%B7%E5%A7%8B%E9%A1%B5/cgcggcdgjfmeoemmdpleinicgepijegd" target="_blank">Edge插件</a>和
-            <a href="https://github.com/leon-kfd/Dashboard/releases/tag/v1.0.1-beta.2" target="_blank">桌面应用</a>等各种使用方式。</p>
+          <p style={{marginBottom: 0}}>完全自定义配置的浏览器导航首页面板，支持自定义添加天气、时钟、搜索、收藏等各种物料组件，可编辑删除组件与其各种样式或功能属性。响应式设计，可自定义随机壁纸背景图或动态壁纸。</p>
+          <details>
+            <summary>预览组件</summary>
+            <ul className="howdy-component">
+              {
+                howdzDashboard.map(item => {
+                  return (
+                    <li key={item.title}>
+                      <a href={item.link} target="_blank">{item.title}</a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </details>
         </section>
       </article>
 
@@ -140,14 +165,14 @@ export default () => {
             <summary>预览组件</summary>
             <ul className="howdy-component">
               {
-              howdyComponent.map(item => {
-                return (
-                  <li key={item.title}>
-                    <a href={item.link} target="_blank">{item.title}</a>
-                    <p>{item.text}</p>
-                  </li>
-                )
-              })
+                howdyComponent.map(item => {
+                  return (
+                    <li key={item.title}>
+                      <a href={item.link} target="_blank">{item.title}</a>
+                      <p>{item.text}</p>
+                    </li>
+                  )
+                })
               }
             </ul>
           </details>
